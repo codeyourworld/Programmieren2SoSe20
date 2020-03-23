7675ae0feadbe2e24e9e4f20d6385d1cba115a82
codeyourworld <stephanie.boehning@haw-hamburg.de>
2020-03-23 15:01:54 +0100

    Game changed, ufos move in a loop

--- ProgrammierenS20/01Vorlesung/ufogame/Game.java
+++ ProgrammierenS20/01Vorlesung/ufogame/Game.java
@@ -6,47 +6,50 @@ import view.GameFrameWork;
 import view.ITickableListener;
 
 public class Game implements ITickableListener {
-	
-	//Idea: we want to have multiple instances of an ufo and of a projectile
+
+	// Idea: we want to have multiple instances of an ufo and of a projectile
 	private ArrayList<Projectile> projectiles = new ArrayList<>();
-	private Ufo [] ufos = new Ufo[10];
+	private ArrayList<Ufo> ufos = new ArrayList<>();
+//	private Ufo [] ufos = new Ufo[10];
 //	private Projectile [] projectiles = new Projectile[30];
 	private Ship ship;
 	private int screenWidth = 900;
 	private int screenHeight = 700;
 	private GameFrameWork frameWork = new GameFrameWork();
-	
-	
+
 	/**
 	 * Initiates everything for the game. Multiple ufos and a ship are created.
 	 */
 	public void init() {
 		frameWork.setSize(screenWidth, screenHeight);
-		
-		ship = new Ship(screenWidth/2, 4 * screenHeight / 5, screenWidth / 20, screenWidth / 20, 
+
+		ship = new Ship(screenWidth / 2, 4 * screenHeight / 5, screenWidth / 20, screenWidth / 20,
 				"01Vorlesung\\assets\\ship23.png");
 		frameWork.addGameObject(ship);
-		
-		Ufo ufo = new Ufo(-20, screenHeight / 5, screenWidth / 10, screenWidth / 10, 1, 
+
+		Ufo ufo = new Ufo(-20, screenHeight / 5, screenWidth / 10, screenWidth / 10, 1,
 				"01Vorlesung\\assets\\ufo20.png");
-		ufos[0] = ufo;
+		ufos.add(ufo);
 		frameWork.addGameObject(ufo);
-		
-        for(int i = 1; i < ufos.length; i++) {
-            ufos[i] = new Ufo(ufos[i-1].getX() - 20, ufos[0].getY(), ufos[0].getWidth(), 
-                    ufos[0].getHeight(), ufos[0].getSpeed(), ufos[0].getImagePath());
-            frameWork.addGameObject(ufos[i]);
-        }
+
+		for (int i = 1; i < 10; i++) {
+			ufos.add(new Ufo(ufos.get(i - 1).getX() - 200, ufos.get(0).getY(), ufos.get(0).getWidth(),
+					ufos.get(0).getHeight(), ufos.get(0).getSpeed(), ufos.get(0).getImagePath()));
+			frameWork.addGameObject(ufos.get(i));
+		}
+
+		frameWork.addTick(this);
+
 	}
-	
+
 	public void shoot() {
-		//create a projectile
-		Projectile projectile = new Projectile(ship.getX(), ship.getY(), 
-				ship.getWidth()/2, ship.getWidth()/2, 3, "01Vorlesung\\assets\\projectile06.png");
-		frameWork.addGameObject(projectile);
+		// create a projectile
+		Projectile projectile = new Projectile(ship.getX(), ship.getY(), ship.getWidth() / 2, ship.getWidth() / 2, 3,
+				"01Vorlesung\\assets\\projectile06.png");
 		projectiles.add(projectile);
-		
-		
+
+		frameWork.addGameObject(projectile);
+
 //		for(int i = 0; i < 1000; i++) {
 //			projectiles.add(projectile);
 //		}
@@ -54,24 +57,30 @@ public class Game implements ITickableListener {
 //		for(Projectile p : projectiles) {
 //			System.out.println(p.getImagePath());
 //		}
-		//Variante Array
-		//projectiles[0] = projectile;
-		
-		//projectiles.get(0).getWidth();
-		//Variante Array
-		//projectiles[0].getWidth();
-		
-		//projectiles.size();
-		//Variante Array
-		//projectiles.lenght
+		// Variante Array
+		// projectiles[0] = projectile;
+
+		// projectiles.get(0).getWidth();
+		// Variante Array
+		// projectiles[0].getWidth();
+
+		// projectiles.size();
+		// Variante Array
+		// projectiles.lenght
 	}
 
 	@Override
 	public void tick(long elapsedTime) {
-		for(Ufo ufo : ufos) {
+		for (Ufo ufo : ufos) {
 			ufo.move();
 		}
-		
+		if (ufos.get(0).getX() > screenWidth) {
+			frameWork.removeGameObject(ufos.get(0));
+			ufos.remove(0);
+			ufos.add(new Ufo(ufos.get(ufos.size() - 1).getX() - 200, ufos.get(0).getY(), ufos.get(0).getWidth(),
+					ufos.get(0).getHeight(), ufos.get(0).getSpeed(), ufos.get(0).getImagePath()));
+			frameWork.addGameObject(ufos.get(ufos.size() - 1));
+		}
 	}
 
 }
