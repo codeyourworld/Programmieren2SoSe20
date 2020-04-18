bc8f8d46c43938df961bd5fa837bc6e080bcd6e3
GrumpyEric <55453689+GrumpyEric@users.noreply.github.com>
2020-04-18 16:47:31 +0200

    lol

---
+++ ProgrammierenS20/01Vorlesung/module-info.java
@@ -0,0 +1,15 @@
+module ProgrammierenS20 {
+	exports view;
+	exports ufogame;
+	exports tickets;
+	exports tests;
+	exports regextool;
+	exports interfaceBsp;
+	exports playSound;
+	exports draw;
+	exports ufoStephi;
+	exports abstractClasses;
+	exports email;
+
+	requires java.desktop;
+}
