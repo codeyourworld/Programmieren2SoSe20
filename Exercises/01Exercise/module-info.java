8b3d430a5e3a50210d20cf7286aff1a7ad80f260
BigBadSheep <j.antich11@gmai.com>
2020-04-20 10:29:34 +0200

    no message

---
+++ Exercises/01Exercise/module-info.java
@@ -0,0 +1,22 @@
+/**
+ * 
+ */
+/**
+ * @author jesus
+ *
+ */
+module Git {
+	exports tickets;
+	exports ufogame;
+	exports regextool;
+	exports draw;
+	exports ufoStephi;
+	exports abstractClasses;
+	exports view;
+	exports movable;
+	exports tests;
+	exports interfaceBsp;
+	exports playSound;
+	exports animal;
+	exports email;
+}
