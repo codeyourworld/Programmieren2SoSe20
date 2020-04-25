b50c0c24cd96030ae78709ebcb127256ea401e3d
magdalucreteanu <magda.lucreteanu@gmail.com>
2020-04-25 17:42:27 +0200

    Aufgabe 6 Account + HashSet

---
+++ ProgrammierenS20/05Vorlesung/aufgaben/Account.java
@@ -0,0 +1,63 @@
+package aufgaben;
+
+public class Account {
+	private String userName;
+	private long mobile;
+
+	public Account(String userName, long mobile) {
+		super();
+		this.userName = userName;
+		this.mobile = mobile;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public long getMobile() {
+		return mobile;
+	}
+
+	public void setMobile(long mobile) {
+		this.mobile = mobile;
+	}
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + (int) (mobile ^ (mobile >>> 32));
+		result = prime * result + ((userName == null) ? 0 : userName.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) {
+			return true;
+		}
+		if (obj == null) {
+			return false;
+		}
+		if (getClass() != obj.getClass()) {
+			return false;
+		}
+		Account other = (Account) obj;
+		if (mobile != other.mobile) {
+			return false;
+		}
+		if (userName == null) {
+			if (other.userName != null) {
+				return false;
+			}
+		} else if (!userName.equals(other.userName)) {
+			return false;
+		}
+		return true;
+	}
+
+}
---
+++ ProgrammierenS20/05Vorlesung/aufgaben/AccountMain.java
@@ -0,0 +1,16 @@
+package aufgaben;
+
+import java.util.HashSet;
+
+public class AccountMain {
+
+	public static void main(String[] args) {
+		HashSet<Account> set = new HashSet<Account>();
+		set.add(new Account("Micky", 1756890000L));
+		set.add(new Account("Taz", 6666660000L));
+		set.add(new Account("Bilbo", 1234560000L));
+		set.add(new Account("Micky", 1756890000L));
+		System.out.println(set.size());
+	}
+
+}
