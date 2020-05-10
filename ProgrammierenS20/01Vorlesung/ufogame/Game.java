4049fcbc9117c03a96957d6352c88d1dd87557b6
codeyourworld <stephanie.boehning@haw-hamburg.de>
2020-05-10 20:13:38 +0200

    Stephis Feedback

--- ProgrammierenS20/01Vorlesung/ufogame/Game.java
+++ ProgrammierenS20/01Vorlesung/ufogame/Game.java
@@ -24,14 +24,12 @@ public class Game implements ITickableListener, IKeyboardListener {
 	 */
 	public void init() {
 		frameWork.setSize(screenWidth, screenHeight);
-		frameWork.setBackground(new Background("01Vorlesung\\assets\\space14.jpg"));
-		
-		ship = new Ship(screenWidth / 2, 4 * screenHeight / 5, screenWidth / 9, screenWidth / 9,
-				"01Vorlesung\\assets\\ship23.png");
+		frameWork.setBackground(new Background("Images\\space.jpg"));
+
+		ship = new Ship(screenWidth / 2, 4 * screenHeight / 5, screenWidth / 9, screenWidth / 9, "Images\\pingu.jpg");
 		frameWork.addGameObject(ship);
 
-		Ufo ufo = new Ufo(-20, screenHeight / 5, screenWidth / 10, screenWidth / 19, 1,
-				"01Vorlesung\\assets\\ufo20.png");
+		Ufo ufo = new Ufo(-20, screenHeight / 5, screenWidth / 10, screenWidth / 19, 1, "Images\\virus.png");
 		ufos.add(ufo);
 		frameWork.addGameObject(ufo);
 
@@ -48,12 +46,13 @@ public class Game implements ITickableListener, IKeyboardListener {
 
 	public void shoot() {
 		// create a projectile
-		Projectile projectile = new Projectile(ship.getX() + ship.getWidth()/4, 
-				ship.getY() - ship.getWidth() / 2, ship.getWidth() / 2, ship.getWidth() / 2, 3,
-				"01Vorlesung\\assets\\projectile06.png");
+		Projectile projectile = new Projectile(ship.getX() + ship.getWidth() / 4, ship.getY() - ship.getWidth() / 2,
+				ship.getWidth() / 2, ship.getWidth() / 2, 3, "Images\\Netflix.png");
 		projectiles.add(projectile);
 
 		frameWork.addGameObject(projectile);
+
+	}
 
 //		for(int i = 0; i < 1000; i++) {
 //			projectiles.add(projectile);
@@ -62,16 +61,59 @@ public class Game implements ITickableListener, IKeyboardListener {
 //		for(Projectile p : projectiles) {
 //			System.out.println(p.getImagePath());
 //		}
-		// Variante Array
-		// projectiles[0] = projectile;
+	// Variante Array
+	// projectiles[0] = projectile;
 
-		// projectiles.get(0).getWidth();
-		// Variante Array
-		// projectiles[0].getWidth();
+	// projectiles.get(0).getWidth();
+	// Variante Array
+	// projectiles[0].getWidth();
 
-		// projectiles.size();
-		// Variante Array
-		// projectiles.lenght
+	// projectiles.size();
+	// Variante Array
+	// projectiles.lenght
+
+	// Aufgabe 2: Method that checks for the Collision between projectile and Ufo.
+	public boolean checkCollision() {
+
+		//TODO du fragst nur das 0. Element ab. es werden aber wahrscheinlich andere Ufos mit Projektilen kollidieren
+		//TODO nutze eine verschachtelte for Schleife fuer alle Projektile und alle Ufos
+		if (ufos.get(0).getX() < projectiles.get(0).getX() + projectiles.get(0).getWidth() && // Check if the
+																								// Coordinates from the
+																								// Ufo overlap with the
+																								// coordinates from the
+																								// bullet
+				(ufos.get(0).getY() < projectiles.get(0).getY() + projectiles.get(0).getHeight()) &&
+
+				(ufos.get(0).getX() + ufos.get(0).getWidth() > projectiles.get(0).getX()) &&
+
+				(ufos.get(0).getY() + ufos.get(0).getHeight() > projectiles.get(0).getY())) {
+
+			// System.out.println("collision detected");
+
+			ufos.get(0).setCollided(true); // Change the isCollided attribute from the Ufo and projectile class to True
+			projectiles.get(0).setCollided(true);
+
+			// Aufgabe 3: Delete both objects if Ufo and Projectile collide.
+			if (ufos.get(0).isCollided() == true) { // Ich glaube ich habe die isCollided und setCollided Attribute
+													// nicht verstanden--
+				frameWork.removeGameObject(ufos.get(0)); // Theoretisch glaube ich das soll funktionieren aber irgendwie
+															// macht die methode nicths--
+				//TODO die ufos musst du entfernen und wieder neue hinzu fügen
+				// ufos.remove(ufos.get(0)); //Weder verschwinden noch "collision detected"
+				// printen.
+			}
+
+			if (projectiles.get(0).isCollided() == true) {
+				frameWork.removeGameObject(projectiles.get(0));
+				projectiles.remove(projectiles.get(0));
+			}
+
+			// System.out.println("Collision detected");
+			// boolean Collision = true;
+		}
+
+		return true;
+
 	}
 
 	@Override
@@ -86,25 +128,43 @@ public class Game implements ITickableListener, IKeyboardListener {
 					ufos.get(0).getHeight(), ufos.get(0).getSpeed(), ufos.get(0).getImagePath()));
 			frameWork.addGameObject(ufos.get(ufos.size() - 1));
 		}
-		
-		for(Projectile p: projectiles) {
-            p.move();
-        }
-		
-		//TODO check size of list
-//			frameWork.removeGameObject(projectiles.get(0));
+
+		for (Projectile p : projectiles) {
+			p.move();
+		}
+
+		// Aufgabe 1: Remove Projectile from Array.
+		// TODO strg + shift + f = formatiert deinen code ordentlich
+		if (projectiles.size() >= 1) {
+			if (projectiles.get(0).getY() < 0) {
+				frameWork.removeGameObject(projectiles.get(0));
+				projectiles.remove(0);
+			}
+		}
+
+		// Check if the Projectile was actually deleted!
+		int size = projectiles.size();
+		System.out.println("Ammount of Projectiles in Array: " + size);
+
+		/*
+		 * if (checkCollision()==true){ System.out.println("collision"); }
+		 */
+
+		// checkCollision(); // Trying to run checkCollision ends up in an error
+		// message.
+
 	}
 
 	@Override
 	public int[] getKeys() {
-		int [] keys = {KeyEvent.VK_SPACE};
+		int[] keys = { KeyEvent.VK_SPACE };
 		return keys;
 	}
 
 	@Override
 	public void keyDown(int key) {
 		shoot();
-		
+
 	}
 
 }
