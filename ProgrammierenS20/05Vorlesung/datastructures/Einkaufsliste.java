cd74dbb99466fc0ce3e66afcbcb99c902b0841f8
laila-cool <34324782+laila-cool@users.noreply.github.com>
2020-05-14 19:01:20 +0200

    HA aus Datenstruktur fertig

--- ProgrammierenS20/05Vorlesung/datastructures/Einkaufsliste.java
+++ ProgrammierenS20/05Vorlesung/datastructures/Einkaufsliste.java
@@ -1,5 +1,31 @@
 package datastructures;
 
 public class Einkaufsliste {
+	
+	String article;
+	String menge;
+	
+	public Einkaufsliste(String article,String menge) {
+		this.article = article;
+		this.menge = menge;
+	}
+	
+	public void setArticle(String article) {
+		this.article = article;
+	}
+	
+	public void setMenge(String menge) {
+		this.menge = menge;
+	}
+	
+	public String getArticle() {
+		return article;
+	}
+	
+	public String getMenge() {
+		return menge;
+	}
+	
+	
 
 }
