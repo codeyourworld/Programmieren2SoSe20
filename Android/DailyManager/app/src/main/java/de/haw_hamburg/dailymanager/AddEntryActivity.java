7a15ee1fabac6c3e1427ae78db2cd3dd7ed46160
magdalucreteanu <magda.lucreteanu@gmail.com>
2020-06-07 21:15:44 +0200

    Android design. Daily App und Quiz

---
+++ Android/DailyManager/app/src/main/java/de/haw_hamburg/dailymanager/AddEntryActivity.java
@@ -0,0 +1,14 @@
+package de.haw_hamburg.dailymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_entry);
+    }
+}
---
+++ Android/DailyManager/app/src/main/java/de/haw_hamburg/dailymanager/MainActivity.java
@@ -0,0 +1,29 @@
+package de.haw_hamburg.dailymanager;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton floatingActionButton = findViewById(R.id.floatingActionButton);
+        floatingActionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(MainActivity.this, "Want to add a new entry", Toast.LENGTH_LONG).show();
+                Intent intent = new Intent(MainActivity.this, AddEntryActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
